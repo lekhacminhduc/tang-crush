9127a9a9c4dc5fb9d08f7a3a34182f976663b085
lekhacminhduc <94516305+lekhacminhduc@users.noreply.github.com>
2021-11-23 23:52:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối mai 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100069783428161' //link mess của các bạn. VD: https://m.me/nam.nodemy
+     //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
